811b3f2a7fb7e80049fd64e3efcb0e616e367ed0
hhh <hhh@-1>
2024-05-13 03:19:23 +0300

    Добавьте файлы проекта.

---
+++ lab12_bebin.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+int findMaxElement(int array[], int size) {
+    int max = array[0]; 
+
+    for (int i = 1; i < size; ++i) {
+        if (array[i] > max) {
+            max = array[i];
+        }
+    }
+
+    return max;
+}
+
+int main() {
+    setlocale(LC_ALL, "Ukrainian");
+    const int SIZE = 10;
+    int divisors[SIZE];
+
+    int number = 100;
+
+    int count = 0;
+    for (int i = 1; i <= number; ++i) {
+        if (number % i == 0) {
+            divisors[count] = i;
+            count++;
+        }
+    }
+
+    int maxElement = findMaxElement(divisors, count);
+
+    cout << "Максимальний дiльник числа " << number << ": " << maxElement << endl;
+
+    return 0;
+}
